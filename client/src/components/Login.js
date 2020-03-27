1ff803989a8f3bcfd9bd9ef315c7593195a00201
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-27 11:20:17 -0400

    Initial login form setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChanges = e => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setLoginData({
+      username: "",
+      password: ""
+    });
+  };
+
   return (
-    <>
+      <div className = "loginform">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+        <form onSubmit = {handleSubmit}>
+        <input
+            type="text"
+            name="username"
+            value={loginData.username}
+            onChange={handleChanges}
+            placeholder = "Username"
+          />
+          <br/>
+          <input
+            type="password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChanges}
+            placeholder = "Password"
+          />
+          <br/>
+          <button>Log in</button>
+        </form>
+      </div>
   );
 };
 
